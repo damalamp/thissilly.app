e382226d5c686dad9bc1d01331facbcb15a63a45
damalamp <sillydamalamp@gmail.com>
2022-12-23 12:16:14 -0500

    Fix form class name

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
 
+    document.querySelector("#linkCreateAccount").addEventListener("click", () => {
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden")
+    })
 })
