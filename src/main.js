2816b0a4a1c264c22dfde28e3b417375b100397a
damalamp <sillydamalamp@gmail.com>
2022-12-23 12:20:54 -0500

    fix form name

--- src/main.js
+++ src/main.js
@@ -6,4 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
         loginForm.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden")
     })
+
+    document.querySelector("#linkLogin").addEventListener("click", () => {
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden")
+    })
+
 })
